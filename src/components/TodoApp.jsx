41eeeda2a81fc665c63cd8d733a0fd52e19bfc0a
getssh <gtamrat33@gmail.com>
2023-03-29 01:20:56 +0300

    Update imported components and render

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,12 +1,15 @@
 import Header from '@/components/Header';
 import TodosLogic from '@/components/TodosLogic';
+import TodoLists from './TodosList';
 
 const TodoApp = () => {
   return (
-    <>
-      <Header />
-      <TodosLogic />
-    </>
+    <section className="container">
+      <div className="wrapper">
+        <Header />
+        <TodoLists />
+      </div>
+    </section>
   );
 };
 export default TodoApp;
