4de5aa9e26ad1c68e7adf737ddd4156c9efb807f
getssh <gtamrat33@gmail.com>
2023-03-28 21:34:48 +0300

    Add more components

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+    
+const TodoApp = () => {
+  return (
+    <React.Fragment>
+      <h1>Hello world!</h1>
+      <p>I am in a React Component!</p>
+    </React.Fragment>
+  );
+};
+export default TodoApp;
